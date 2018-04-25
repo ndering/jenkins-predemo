5b1e49869b2caddf620c3e5c28bdb7650db8cbf4
Noah <noahdering@gmail.com>
2018-04-25 15:58:58 -0400

    Add files via upload

---
+++ JenkinsPredemo/src/main/java/main/TestServlet.java
@@ -0,0 +1,27 @@
+package main;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/test")
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public TestServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.write("Application successfully build using Jenkins, and deployed to an AWS EC2 instance!");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+}
